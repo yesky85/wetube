7e810a2ea5b0b65c5127b14ecd41af0cb6eea813
kec <57247368+yesky85@users.noreply.github.com>
2020-02-16 00:59:22 +0900

    #2.6 Express Core: Middlewares part One

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const handleHome = (req, res) => res.send('Hello from my home');
 
 const handleProfile = (req, res) => res.send('You are on my profile');
 
+const betweenHome = (req, res, next) => {
+  console.log(`between`);
+  next();
+};
+
+app.use(betweenHome);
+
 app.get('/', handleHome);
 
 app.get('/profile', handleProfile);
