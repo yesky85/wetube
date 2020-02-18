1b9e4add7b1593936b5205445c3e22d78895ead6
kec <57247368+yesky85@users.noreply.github.com>
2020-02-18 18:17:52 +0900

    #2.18 Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,22 @@
 export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
-export const search = (req, res) =>
-  res.render('search', { pagaTitle: 'Search' });
+
+export const search = (req, res) => {
+  // const searchingBy = req.query.term;
+  // res.render('search', { pagaTitle: 'Search', searchingBy: searchingBy });
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render('search', { pagaTitle: 'Search', searchingBy });
+};
+
 export const upload = (req, res) =>
   res.render('upload', { pagaTitle: 'Upload' });
+
 export const videoDetail = (req, res) =>
   res.render('videoDetail', { pagaTitle: 'Video Detail' });
+
 export const editVideo = (req, res) =>
   res.render('editVideo', { pagaTitle: 'Edit Video' });
+
 export const deleteVideo = (req, res) =>
   res.render('deleteVideo', { pagaTitle: 'Delete Video' });
