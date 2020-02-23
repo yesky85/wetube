c41971d773945a9456903f715611d46b106dcad6
kec <57247368+yesky85@users.noreply.github.com>
2020-02-23 23:53:20 +0900

    #3.4 Comment Model

--- models/Video.js
+++ models/Video.js
@@ -19,6 +19,12 @@ const VideoSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  comment: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      Ref: 'Comment',
+    },
+  ],
 });
 
 const model = mongoose.model('Video', VideoSchema);
