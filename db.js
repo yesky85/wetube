482ce7348fd22e7ca9c6f0d96df963ed2fdeb720
kec <57247368+yesky85@users.noreply.github.com>
2020-02-29 20:19:45 +0900

    #6.2 Local Authentication with Passport part Two

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGO_URL, {
 const db = mongoose.connection;
 
 const handleOpen = () => console.log('✅ Connected to DB');
-const handleError = error => console.log('❌ Error on DB Connection: ${error}');
+const handleError = error => console.log(`❌ Error on DB Connection: ${error}`);
 
 db.once('open', handleOpen);
 db.on('error', handleError);
