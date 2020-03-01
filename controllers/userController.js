849a291d7a81087bac8079c95364464db47ae1fd
kec <57247368+yesky85@users.noreply.github.com>
2020-03-01 21:34:29 +0900

    #6.12 Facebook Log In part Three

--- controllers/userController.js
+++ controllers/userController.js
@@ -67,13 +67,29 @@ export const postGithubLogin = (req, res) => {
 
 export const facebookLogin = passport.authenticate('facebook');
 
-export const facebookLoginCallback = (
-  accessToken,
-  refreshToken,
-  profile,
-  cb,
-) => {
-  console.log(accessToken, refreshToken, profile, cb);
+export const facebookLoginCallback = async (_, __, profile, cb) => {
+  const {
+    _json: { id, name },
+  } = profile;
+  try {
+    const email = 'email@gmail.com';
+    const user = await User.findOne({ email });
+    if (user) {
+      user.facebookId = id;
+      user.avatarUrl = `https://graph.facebook.com/${id}/picture?type=large`;
+      user.save();
+      return cb(null, user);
+    }
+    const newUser = await User.create({
+      email: 'yesky85@facebook.com',
+      name,
+      facebookId: id,
+      avatarUrl: `https://graph.facebook.com/${id}/picture?type=large`,
+    });
+    return cb(null, newUser);
+  } catch (error) {
+    return cb(error);
+  }
 };
 
 export const postFacebookLogin = (req, res) => {
