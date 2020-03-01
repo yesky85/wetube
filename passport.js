2ed79833006be6aeb70be9d562433ccdf403fbb3
kec <57247368+yesky85@users.noreply.github.com>
2020-03-01 11:32:03 +0900

    #6.6 Github Log In part One

--- passport.js
+++ passport.js
@@ -1,7 +1,18 @@
 import passport from 'passport';
+import GithubStrategy from 'passport-github';
 import User from './models/User';
+import { githubLoginCallback } from './controllers/userController';
 
 passport.use(User.createStrategy());
+
+passport.use(
+  new GithubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRT,
+    callbackURL: 'http://localhost:4000/auth/github/callback',
+  }),
+  githubLoginCallback,
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
