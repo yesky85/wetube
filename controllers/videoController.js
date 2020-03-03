b118d8116a7e79955cbf3510ff62c10f6d2427f0
kec <57247368+yesky85@users.noreply.github.com>
2020-03-04 08:25:48 +0900

    #7.5 Bug fixes

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -75,7 +75,7 @@ export const getEditVideo = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
-    if (Video.creator !== req.user.id) {
+    if (video.creator != req.user.id) {
       throw Error();
     } else {
       res.render('editVideo', { pageTitle: `Edit ${video.title}`, video });
@@ -105,7 +105,8 @@ export const deleteVideo = async (req, res) => {
     params: { id },
   } = req;
   try {
-    if (Video.creator !== req.user.id) {
+    const video = await Video.findById(id);
+    if (video.creator != req.user.id) {
       throw Error();
     } else {
       await Video.findOneAndRemove({ _id: id });
