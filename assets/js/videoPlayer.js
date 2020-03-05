c79eda1513d019b19dcd399385bc89d9d732c4a1
kec <57247368+yesky85@users.noreply.github.com>
2020-03-05 11:34:57 +0900

    #8.2 Mute / Unmute

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,17 +1,33 @@
 const videoContainer = document.getElementById('jsVideoPlayer');
 const videoPlayer = document.querySelector('#jsVideoPlayer video');
 const playBtn = document.getElementById('jsPlayButton');
+const volumeBtn = document.getElementById('jsVolumeButton');
 
-function handlePlayClick(event) {
+function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+  }
+}
+
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
 }
 
 function init() {
   playBtn.addEventListener('click', handlePlayClick);
+  volumeBtn.addEventListener('click', handleVolumeClick);
 }
 
-init();
+if (videoContainer) {
+  init();
+}
