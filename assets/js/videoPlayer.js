e3d3b29036673fe811fc3f7c0b83f3dac76c5de3
kec <57247368+yesky85@users.noreply.github.com>
2020-03-06 13:02:37 +0900

    #8.4 Total Time and Current Time

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,6 +3,8 @@ const videoPlayer = document.querySelector('#jsVideoPlayer video');
 const playBtn = document.getElementById('jsPlayButton');
 const volumeBtn = document.getElementById('jsVolumeButton');
 const fullScreenBtn = document.getElementById('jsFullScreen');
+const currentTime = document.getElementById('currentTime');
+const totalTime = document.getElementById('totalTime');
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -38,10 +40,39 @@ function handleFullScreenClick() {
   fullScreenBtn.addEventListener('click', handleResizeScreenClick);
 }
 
+const formatDate = seconds => {
+  const secondsNumber = parseInt(seconds, 10);
+  let hours = Math.floor(secondsNumber / 3600);
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (totalSeconds < 10) {
+    totalSeconds = `0${totalSeconds}`;
+  }
+  return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+function setCurrentTime() {
+  currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+}
+
+function setTotalTIme() {
+  const totalTimeString = formatDate(videoPlayer.duration);
+  totalTime.innerHTML = totalTimeString;
+  setInterval(setCurrentTime, 1000);
+}
+
 function init() {
   playBtn.addEventListener('click', handlePlayClick);
   volumeBtn.addEventListener('click', handleVolumeClick);
   fullScreenBtn.addEventListener('click', handleFullScreenClick);
+  videoPlayer.addEventListener('loadedmetadata', setTotalTIme);
 }
 
 if (videoContainer) {
