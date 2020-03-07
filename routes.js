325035ec6f1f3bc3a009c30351b9375f7249975f
kec <57247368+yesky85@users.noreply.github.com>
2020-03-07 12:23:11 +0900

    #10.0 API Registering a View part One

--- routes.js
+++ routes.js
@@ -32,6 +32,11 @@ const GITHUB_CALLBACK = '/auth/github/callback';
 const FACEBOOK = '/auth/facebook';
 const FACEBOOK_CALLBACK = '/auth/facebook/callback';
 
+// API
+
+const API = '/api';
+const REGISTER_VIEW = '/:id/view';
+
 const routes = {
   home: HOME,
   join: JOIN,
@@ -76,6 +81,8 @@ const routes = {
   me: ME,
   facebook: FACEBOOK,
   facebookCallback: FACEBOOK_CALLBACK,
+  api: API,
+  registerView: REGISTER_VIEW,
 };
 
 export default routes;
