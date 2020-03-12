da2f68c407039fe0f77f4d582a644b69e2feb23e
kec <57247368+yesky85@users.noreply.github.com>
2020-03-12 16:48:08 +0900

    #11.6 Deploying to Heroku part Two

--- src/db.js
+++ src/db.js
@@ -3,11 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL_ATLAS, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.PRODUCTION ? process.env.MONGO_URL_ATLAS : process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  },
+);
 
 const db = mongoose.connection;
 
